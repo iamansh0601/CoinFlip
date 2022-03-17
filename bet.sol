7cfaafebe6dd9a945735d92db8afd3ef129f9bb6
iamansh0601 <59844314+iamansh0601@users.noreply.github.com>
2022-03-18 00:13:39 +0530

    Update bet.sol

--- bet.sol
+++ bet.sol
@@ -1,68 +1,106 @@
+
+/* I was not able to implement Harmony VRF so i have chosen a result value by myself. */
 pragma solidity 0.5.1;
 contract CoinFlip{
-    mapping(uint => uint) public balance;
-    mapping(uint => uint) public busy_avail ; // either 0 or 1 o represents free and 1 represents busy
-    struct userData{
-        uint user;
-        uint bet;
+    mapping(address=>uint) public balance; // mapping to store balance of each user
+    /* mapping to find whether a person has undecided bet or not. 
+       1 indicates he has a undecided bet while 0 indicates he is free to go for the bet.*/
+         mapping(address=>uint) public busy;
+    mapping(address=>uint) public _id; //Mapping to store id of the user with its address as key.
+    uint public w; //to store result of the bet
+    uint public peopleCount = 0; //to keep a count of the number of people participating in the bet.
+    /*Line 11-15 is to ensure that our reward get block does not gets executed before 60 seconds
+     after the code gets deployed for the first time*/
+    uint openTime = block.timestamp; 
+    modifier onlyAfterSomeTime(){
+        require (block.timestamp>=openTime+60);
+        _;
+    }
+    //Struct to store user's data like his address, and id to access his record, amount he has betted and his bet.
+    struct Player{
+        address user;
+        uint id;
         uint amount;
-    }
-    userData[] user_data;
-    uint count = 0;
-
-    //To allow a user to place a bet adn hence accept an amount as an integer and a bet as 0/1
-    //Things to do when a bet is placed
-    //Reduce the balance by bet amount of the player
-    //Store the bet, the amount of bet and user in ststuct data type
-    function placeBet(uint amount, uint bet,uint player) public {
-       uint flag = 0; 
-       uint pos =  0;
-       for(uint i=0;i<user_data.length;i++)
-       {
-           if((user_data[i].user) ==(player))
-           {
-               flag=1;
-               pos= i;
-               break;
-           }
-       }
-       if(flag==0)
-       {
-           balance[player] = 100;
-           busy_avail[player] = 0;
-        if(amount<=balance[player] && busy_avail[player]==0 )
-        {  
-            balance[player] = balance[player] - amount;
-            busy_avail[player] = 1;
-            user_data[count] = userData(player,bet,amount);
-        }
-        count++;
-       }
-       else
-       {
-            if(amount<=balance[player] && busy_avail[player]==0 )
-        {  
-            balance[player] = balance[player] - amount;
-            busy_avail[player] = 1;
-            user_data[pos] = userData(player,bet,amount);
-        }
-       }
+        uint bet;
     }
 
-    function rewardBets() public returns(uint) {
-        uint w = 0;
-        for(uint i= 0; i <user_data.length;i++)
+    Player[] public record; // to keep an array of the record of each user
+    //Function which allows user to place bets
+    function placeBet(uint amount, uint bet) public{
+        uint flag = 0 ;  // this variable is used to dtermine whether current user is new one or has been a part of the game previously
+                         // flag = 0 means it is new but flag = 1 means he is previous user.
+        uint pos = 0 ;   // this gives the id or index of the person if he was previously present.
+        
+        if(busy[msg.sender]!=1)  // to check if the curr user has no undecided bet
         {
-            if((user_data[i].bet) == w)
+        for(uint i=0;i<peopleCount;i++) // to check in the record we have maintained if the person with the same address has placed bet before
+        {
+            if((record[i].user) == msg.sender)
             {
-                balance[user_data[i].user] = balance[user_data[i].user] + 2*(user_data[i].amount);
-                busy_avail[user_data[i].user] = 0;
+               flag = 1;  // Current user is previously there
+               pos = i;   // id or index of the user in the record.
+               break;
             }
         }
-        return  balance[user_data[0].user];
+        if(flag==0)  // if the user is new
+        {
+            balance[msg.sender] = 100;  //give him a new joinee token of 100
+            if(amount<=balance[msg.sender]) // if the amount of bet is less than or eaual to the balance of user, then only bet can be placed
+            {
+            record.push(Player(msg.sender,peopleCount,amount,bet)); // to create a new entry in record
+             balance[msg.sender] = balance[msg.sender] - amount;    // to update balance
+            _id[msg.sender] = peopleCount;                          // to allocate an id
+            busy[msg.sender] = 1;                                   // update its status as the user has an undecide bet now
+            peopleCount = peopleCount+1;                            // update number of people in the record
+            }
+        }
+        else{                                                      //if not a new user
+             if(amount<=balance[msg.sender])
+             {
+            record[pos].amount = amount;                           // update the previous amount of bet 
+            record[pos].bet = bet;                                 //update th bet
+             balance[msg.sender] = balance[msg.sender] - amount;   //update the amount
+             }
+        }
+       
+       
     }
-    //function showResults() public view returns(address[] memory) {
-        //for(uint i= 0; i <user_data.length;i++)
-          // return balance;
-    //}
+    }
+
+    //This function gets the value of winnning bet and then updates the value of balances of each user depending on whether hey won or lose
+    function rewardBet()public onlyAfterSomeTime { 
+        /*I could not implemet harmony VRF so i have choosed a winning bet myself to try out 
+        whether other parts of function work or not*/
+        w=0;  
+        // to check all records
+        for(uint i = 0; i<peopleCount ;i++)
+        {
+           if( busy[record[i].user]==1) // to ensure that upon even calling the reward function multiple time value of the reward gets updated only once.
+           {
+           if(record[i].bet == w) //if bet of user is equal to winning bet.
+           {
+               balance[record[i].user] += 2* record[i].amount; //update the amount as per wiing criteria
+               busy[record[i].user] = 0;  // update the staus so that user is now free to place another bet.
+           }
+           else
+           {
+                busy[record[i].user] = 0;  // if he loses just update the status that the result of his bet is decide and now he can place the nextrbet
+           }
+           }
+        }
+    }
 }
+
+    /* function vrf() public view returns (bytes32 result) {
+    uint[1] memory bn;
+    bn[0] = block.number;
+    assembly { 
+      let memPtr := mload(0x40)
+      if iszero(staticcall(not(0), 0xff, bn, 0x20, memPtr, 0x20)) {
+        invalid()
+      }
+      result := mload(memPtr)
+     
+    }
+  }*/
+
